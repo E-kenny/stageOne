daceaf4e4ff9d2d91d2c2241ed3dd89996d6ed17
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-09-10 00:25:22 +0100

    fix: Add Query

--- stageOne/main.go
+++ stageOne/main.go
@@ -10,12 +10,12 @@ import (
 func main() {
   now := time.Now()
   r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
+  r.GET("/api", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
-      "slack_name": "E_kenny",
+      "slack_name": c.Query("slack_name"),
       "current_day": now.Format("Monday"),
       "utc_time": now.Format(time.RFC3339),
-      "track": "backend",
+      "track": c.Query("track"),
       "github_file_url": "https://github.com/E-kenny/stageOne/blob/main/stageOne/main.go",
       "github_repo_url": "https://github.com/E-kenny/stageOne",
       "status_code": http.StatusOK,
