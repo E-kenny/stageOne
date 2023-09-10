72ab272efa0ead3e23804e5521e617478a59ca11
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-09-10 03:44:43 +0100

    fix: Inside Json

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-  now := time.Now()
   
   tz, err := time.LoadLocation("UTC")
 	if err != nil { // Always check errors even if they should not happen.
@@ -19,8 +18,8 @@ func main() {
   r.GET("/api", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
       "slack_name": c.Query("slack_name"),
-      "current_day": now.In(tz).Format("Monday"),
-      "utc_time": now.In(tz).Format(time.RFC3339),
+      "current_day": time.Now().In(tz).Format("Monday"),
+      "utc_time": time.Now().In(tz).Format(time.RFC3339),
       "track": c.Query("track"),
       "github_file_url": "https://github.com/E-kenny/stageOne/blob/main/main.go",
       "github_repo_url": "https://github.com/E-kenny/stageOne",
